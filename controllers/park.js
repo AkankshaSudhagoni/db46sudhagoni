a7a99aa1342ea7af420ba10d94eb792bb8917030
S542046 <77809588+S542046@users.noreply.github.com>
2021-04-14 20:23:20 -0500

    Final Commit

--- controllers/park.js
+++ controllers/park.js
@@ -1,20 +1,20 @@
 var park = require('../models/park');
 // List of all parks
-exports.park_list = async function(req, res) {
-    try{
+exports.park_list = async function (req, res) {
+    try {
         theparks = await park.find();
         res.send(theparks);
-        }
-        catch(err){
-            res.status(500);
-            res.send(`{"error": ${err}}`);
-            }
-        };
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
 // for a specific park.
-exports.park_detail = async function(req, res) {
-    console.log("detail"  + req.params.id)
+exports.park_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
     try {
-        result = await park.findById( req.params.id)
+        result = await park.findById(req.params.id)
         res.send(result)
     } catch (error) {
         res.status(500)
@@ -23,7 +23,7 @@ exports.park_detail = async function(req, res) {
 };
 ;
 // Handle park create on POST.
-exports.park_create_post = async function(req, res) {
+exports.park_create_post = async function (req, res) {
     console.log(req.body)
     let document = new park();
     // We are looking for a body, since POST does not have query parameters.
@@ -33,27 +33,36 @@ exports.park_create_post = async function(req, res) {
     document.parkname = req.body.parkname;
     document.parklocation = req.body.parklocation;
     document.parkarea = req.body.parkarea;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
-        }
-    };
-// Handle park delete form on DELETE.
-exports.park_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: park delete DELETE ' + req.params.id);
+    }
 };
+// Handle park delete on DELETE.
+exports.park_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await park.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
 // Handle park update form on PUT.
-exports.park_update_put = async function(req, res) {
+exports.park_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
     try {
         let toUpdate = await park.findById(req.params.id)
         // Do updates of properties
-        if(req.body.parklocation) toUpdate.parklocation = req.body.parklocation;
-        if(req.body.parkarea) toUpdate.parkarea = req.body.parkarea;
+        if (req.body.parklocation) toUpdate.parklocation = req.body.parklocation;
+        if (req.body.parkarea) toUpdate.parkarea = req.body.parkarea;
         let result = await toUpdate.save();
         console.log("Sucess " + result)
         res.send(result)
@@ -65,13 +74,67 @@ exports.park_update_put = async function(req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.park_view_all_Page = async function(req, res) {
-    try{
-    theparks = await park.find();
-    res.render('park', { title: 'park Search Results', results: theparks });
+exports.park_view_all_Page = async function (req, res) {
+    try {
+        theparks = await park.find();
+        res.render('park', { title: 'park Search Results', results: theparks });
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
-        }
-    };  
+    }
+};
+// Handle a show one view with id specified by query
+exports.park_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await park.findById(req.query.id)
+        res.render('parkdetail',
+            { title: 'park Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a park.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.park_create_Page = async function (req, res) {
+    console.log("create view")
+    try {
+        res.render('parkcreate', { title: 'Park Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a park.
+// query provides the id
+exports.park_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await park.findById(req.query.id)
+        res.render('parkupdate', { title: 'park Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.park_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await park.findById(req.query.id)
+        res.render('parkdelete', { title: 'park Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/park.js
+++ routes/park.js
@@ -4,5 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', park_controlers.park_view_all_Page );
+router.get('/detail', park_controlers.park_view_one_Page);
+router.get('/create', park_controlers.park_create_Page);
+router.get('/update', park_controlers.park_update_Page);
+router.get('/delete', park_controlers.park_delete_Page);
+
 
 module.exports = router;
